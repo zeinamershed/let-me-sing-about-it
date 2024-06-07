f0a93d89f97bdddbb7ace93fdcc06ad36b1bfda7
Andreia Sá <andreiapsa6@gmail.com>
2024-06-07 17:40:24 +0200

    navbar links almost done

--- Let-Me-Sing-About-It/src/App.jsx
+++ Let-Me-Sing-About-It/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
