71a182b8b1b0bc449c0a162abd3ed3391b4f9f6c
DaniDias95 <guiguimu_art@hotmail.com>
2024-06-10 11:29:25 +0100

    all songs page and song detail page working

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,30 @@ import RandomSongPage from './pages/RandomSongPage';
 import FavoriteSongsPage from './pages/FavoriteSongsPage';
 import { Routes, Route } from "react-router-dom";
 import About from '../src/pages/AboutPage'
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { API_URL } from './config';
 import Navbar from './components/Navbar'
 import Footer from './components/Footer';
 import AllSongsPage from './pages/AllSongsPage';
 import OneSongDetails from './pages/OneSongDetails';
 
 function App() {
+  const [songs, setSongs] = useState([]);
 
+  useEffect(() => {
+    const fetchSongs = async () => {
+      try {
+        const { data } = await axios.get(`${API_URL}/songs`);
+        console.log("here is the return from the post", data);
+        setSongs(data);
+      } catch (error) {
+        console.log("there was an error with the post", error);
+      }
+    };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    fetchSongs();
+  }, []);
 
  /*  function handleDeleteUnit(unitId) {
     const filteredRentals = rentals.filter((rental) => {
@@ -49,13 +47,12 @@ function App() {
     <Navbar />
 			<Routes>
         <Route path='/' element={<HomePage />}/>
-        <Route path='/songs' element={<AllSongsPage/>}/>
+        <Route path='/songs' element={<AllSongsPage songs={songs}/>}/>
 				<Route path='/decades' element={<DecadesPage/>}/>
         <Route path='/random' element={<RandomSongPage/>}/>
         <Route path='/favorites' element={<FavoriteSongsPage/>}/>
         <Route path="/About" element={<About />} />
-        <Route path="/songs/:songId" element={<OneSongDetails/>}
-          />
+        <Route path="/songs/:songId" element={<OneSongDetails songs={songs}/>}/>
 			</Routes>
       
       <br />
--- src/pages/OneSongDetails.jsx
+++ src/pages/OneSongDetails.jsx
@@ -1,50 +1,54 @@
-import React from 'react'
-import { Link, useParams } from "react-router-dom";
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
 
-const OneSongDetails = () => {
-    const { songID } = useParams();
-    const filteredSongs = songs.find((song) => { // try with useEffect so no need of passing props
-      if (song.id == songID) {
-        return true;
-      }
-    });
-  
-    return (
-      <div className="for-all-divs">
-        <div>
-        <img src={filteredSongs.image} alt={filteredSongs.song} />
-        </div>
-        <h2>{filteredSongs.song}</h2>
-        <h4>{filteredSongs.artist}, {filteredSongs.genre}</h4>
-        
-        <p>{filteredSongs.decade}</p>
-        <h4>Lyrics:</h4>
-        <p>{filteredSongs.lyrics}</p>
-        <br/>
-        <hr className='hr-random'/>
+const OneSongDetails = ({ songs }) => {
+	const { songId } = useParams();
+	const filteredSong = songs.find((song) => song.id === songId);
 
-					<h2>Sing it here!</h2>
-					<div className="video-container">
-						<iframe
-							width="750"
-							height="425"
-							src={filteredSongs.videoUrl}
-							title="YouTube video player"
-							frameBorder="0"
-							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-							allowFullScreen
-						></iframe>
-					</div>
-            <   div>
-					<Link to="/">
-						<button className='random-btn'>Back</button>
-					</Link>
-                    {/* <Link to={`/edit-song/${filteredSongs.id}`}>
-                     <button className="edit-btn">Edit song</button>
-                    </Link> */}
-                </div>  
-      </div>
-    );
-  };
+	if (!filteredSong) {
+		return <p>Song not found!</p>;
+	}
 
-export default OneSongDetails
+	return (
+		<div className="for-all-divs">
+			<div>
+				<img
+					src={filteredSong.image}
+					alt={filteredSong.song}
+				/>
+			</div>
+			<h2>{filteredSong.song}</h2>
+			<h4>
+				{filteredSong.artist}, {filteredSong.genre}
+			</h4>
+			<p>{filteredSong.decade}</p>
+			<h4>Lyrics:</h4>
+			<p>{filteredSong.lyrics}</p>
+			<br />
+			<hr className="hr-random" />
+
+			<h2>Sing it here!</h2>
+			<div className="video-container">
+				<iframe
+					width="650"
+					height="400"
+					src={filteredSong.videoUrl}
+					title="YouTube video player"
+					frameBorder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+					allowFullScreen
+				></iframe>
+			</div>
+			<div>
+				<Link to="/songs">
+					<button className="random-btn">Back</button>
+				</Link>
+				{/* <Link to={`/edit-song/${filteredSong.id}`}>
+         <button className="edit-btn">Edit song</button>
+        </Link> */}
+			</div>
+		</div>
+	);
+};
+
+export default OneSongDetails;
