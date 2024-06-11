a6cc6019b1eecde0e0a4aa36b0bd66e53e9266ae
DaniDias95 <guiguimu_art@hotmail.com>
2024-06-11 19:51:10 +0100

    mvp almost done, just missing edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,15 @@
-import githublogo from '../assets/images/githublogo.jpg';
-import { Link, NavLink } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-	faFile,
-	faHouse,
-	faCircleInfo,
-} from '@fortawesome/free-solid-svg-icons';
+import githublogo from '../assets/images/githublogo.png';
+import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
     return (
       <div className="footer">
           
          <a href="https://github.com/zeinamershed/let-me-sing-about-it">
-            <img src={githublogo} alt="github-logo"/> 
-         
-         <h5>GitHub Repositories</h5>
+            <img src={githublogo} alt="github-logo"/>                
         </a>
-        <NavLink to="/About"><FontAwesomeIcon icon={faFile} /> <span>About Us</span></NavLink>
+        <div></div>
+        <NavLink to="/About"><span>About Us</span></NavLink>
       </div>
     )
   }
